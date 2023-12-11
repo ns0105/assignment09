225cc829c691775c94a17e988417f4899ac4029d
ns0105 <152585596+ns0105@users.noreply.github.com>
2023-12-11 12:35:44 +0600

    first commit

--- nayema2/akaban/urls.py
+++ nayema2/akaban/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.an),
+    path('home/', views.An_Home, name='home'), 
+    path('account/', views.An_Account, name='account'), 
+    path('offers/', views.An_Offers, name='offers'), 
+    #Categories:
+    path('bs/', views.Best_sellers, name='bestsellers'), 
     
     
 
--- nayema2/akaban/views.py
+++ nayema2/akaban/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
 def an(request):
     return render(request, 'akaban/an.html',)
+def An_Home(request):
+    return render(request, 'akaban/home.html')
+def An_Account(request):
+    return render(request, 'akaban/account.html')
+def An_Categories(request):
+    return render(request, 'akaban/categories.html')
+def An_Offers(request):
+    return render(request, 'akaban/offers.html')
+def Best_sellers(request):
+    return render(request, 'akaban/bestsellers.html')
+
