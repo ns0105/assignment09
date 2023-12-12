841c60fb97c7d8a355a53384708d4b4e1f600aa6
ns0105 <152585596+ns0105@users.noreply.github.com>
2023-12-12 22:37:45 +0600

    first commit

--- nayema2/akaban/views.py
+++ nayema2/akaban/views.py
@@ -9,10 +9,9 @@ def An_Home(request):
     return render(request, 'akaban/home.html')
 def An_Account(request):
     return render(request, 'akaban/account.html')
-def An_Categories(request):
-    return render(request, 'akaban/categories.html')
 def An_Offers(request):
     return render(request, 'akaban/offers.html')
+#Categories:
 def Best_sellers(request):
     return render(request, 'akaban/bestsellers.html')
 
